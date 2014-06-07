ngld/fs2open

commit 3b46ba39e7b335e67d5526d4fcf22d57463aa000

Added multiple scripting features related to messaging as requested by Axem.
git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10756 387891d4-d844-0410-90c0-e4c51a9137d3

code/mission/missionmessage.cpp
@@ -34,6 +34,7 @@
 #include "network/multimsgs.h"
 #include "network/multiutil.h"
 #include "mod_table/mod_table.h"
+#include "parse/scripting.h"
 
 SCP_vector<SCP_string> Builtin_moods;
 int Current_mission_mood;
@@ -321,7 +322,7 @@ int add_avi( char *avi_name )
 	return ((int)Message_avis.size() - 1);
 }
 
-int add_wave( char *wave_name )
+int add_wave( const char *wave_name )
 {
 	int i;
 	message_extra extra; 
@@ -987,10 +988,7 @@ void message_remove_from_queue(message_q *q)
 //
 void message_load_wave(int index, const char *filename)
 {
-	if (index == -1) {
-		Int3();
-		return;
-	}
+	Assertion(index >= 0, "Invalid index passed!");
 
 	if ( Message_waves[index].num >= 0) {
 		return;
@@ -1582,6 +1580,29 @@ void message_queue_process()
 	if ( Message_shipnum >= 0 ) {
 		hud_target_last_transmit_add(Message_shipnum);
 	}
+
+	Script_system.SetHookVar("Name", 's', m->name);
+	Script_system.SetHookVar("Message", 's', buf);
+	Script_system.SetHookVar("SenderString", 's', who_from);
+
+	bool builtinMessage = q->builtin_type != -1;
+	Script_system.SetHookVar("Builtin", 'b', &builtinMessage);
+	if (Message_shipnum >= 0)
+	{
+		object* sender = &Objects[Ships[Message_shipnum].objnum];
+
+		Script_system.SetHookObject("Sender", sender);
+
+		Script_system.RunCondition(CHA_MSGRECEIVED, 0, NULL, sender);
+
+		Script_system.RemHookVar("Sender");
+	}
+	else
+	{
+		Script_system.RunCondition(CHA_MSGRECEIVED);
+	}
+
+	Script_system.RemHookVars(4, "Name", "Message", "SenderString", "Builtin");
 
 all_done:
 	Num_messages_playing++;

code/fred2/sexp_tree.cpp
@@ -2203,7 +2203,8 @@ int sexp_tree::add_default_operator(int op, int argnum)
 				(argnum == 1 && Operators[op].value == OP_INT_TO_STRING) ||
 				(argnum == 3 && Operators[op].value == OP_STRING_GET_SUBSTRING) ||
 				(argnum == 4 && Operators[op].value == OP_STRING_SET_SUBSTRING) ||
-				(argnum == 1 && Operators[op].value == OP_COPY_VARIABLE_FROM_INDEX))
+				(argnum == 1 && Operators[op].value == OP_COPY_VARIABLE_FROM_INDEX) ||
+				(argnum == 1 && Operators[op].value == OP_SCRIPT_EVAL_STRING))
 			{
 
 				int sexp_var_index = get_index_sexp_variable_name(item.text);

code/freespace2/freespace.cpp
@@ -4529,7 +4529,7 @@ void game_frame(bool paused)
 			}
 
 			Script_system.SetHookObject("Self", Viewer_obj);
-			if (!hud_disabled_except_messages() && !(Viewer_mode & (VM_EXTERNAL | VM_DEAD_VIEW | VM_WARP_CHASE | VM_PADLOCK_ANY))) 
+			if (!(Viewer_mode & (VM_EXTERNAL | VM_DEAD_VIEW | VM_WARP_CHASE | VM_PADLOCK_ANY))) 
 			{
 				Script_system.RunBytecode(Script_hudhook);
 				Script_system.RunCondition(CHA_HUDDRAW, '\0', NULL, Viewer_obj);

code/mission/missionmessage.h
@@ -219,6 +219,9 @@ int message_filter_multi(int id);
 // Goober5000
 bool message_filename_is_generic(char *filename);
 
+// m!m
+void message_load_wave(int index, const char *filename);
+
 // Kazan
 // Use these functions with caution as everything else uses indexes... so make sure if you're going to
 // use these there will be no remove_messages called before your message is displayed.

code/parse/scripting.cpp
@@ -80,7 +80,8 @@ flag_def_list Script_actions[] =
 	{"On Primary Fire",			CHA_PRIMARYFIRE,	0},
 	{"On Secondary Fire",		CHA_SECONDARYFIRE,	0},
 	{"On Ship Arrive",			CHA_ONSHIPARRIVE,	0},
-	{"On Beam Collision",		CHA_COLLIDEBEAM,	0}
+	{"On Beam Collision",		CHA_COLLIDEBEAM,	0},
+	{"On Message Received",		CHA_MSGRECEIVED,	0}
 };
 
 int Num_script_actions = sizeof(Script_actions)/sizeof(flag_def_list);

code/parse/scripting.h
@@ -74,6 +74,7 @@ struct image_desc
 #define CHA_COLLIDEBEAM		31
 #define CHA_ONACTION		32
 #define CHA_ONACTIONSTOPPED	33
+#define CHA_MSGRECEIVED		34
 
 // management stuff
 void scripting_state_init();

code/parse/sexp.cpp
@@ -307,7 +307,7 @@ sexp_oper Operators[] = {
 
 	//Other Sub-Category
 	{ "script-eval-num",				OP_SCRIPT_EVAL_NUM,						1,	1,			SEXP_INTEGER_OPERATOR,	},
-	{ "script-eval-string",				OP_SCRIPT_EVAL_STRING,					1,	1,			SEXP_INTEGER_OPERATOR,	},
+	{ "script-eval-string",				OP_SCRIPT_EVAL_STRING,					2,	2,			SEXP_ACTION_OPERATOR,	},
 
 	//Time Category
 	{ "time-ship-destroyed",			OP_TIME_SHIP_DESTROYED,					1,	1,			SEXP_INTEGER_OPERATOR,	},
@@ -21473,24 +21473,65 @@ void multi_sexp_show_hide_jumpnode(bool show)
 int sexp_script_eval(int node, int return_type)
 {
 	int n = node;
-	char *s = CTEXT(n);
-	bool success = false;
-
-	int r = -1;
 
 	switch(return_type)
 	{
 		case OPR_NUMBER:
-			success = Script_system.EvalString(s, "|i", &r, s);
-			break;
+			{
+				char* s = CTEXT(n);
+				int r = -1;
+				bool success = Script_system.EvalString(CTEXT(n), "|i", &r);
+
+				if(!success)
+					Warning(LOCATION, "sexp-script-eval failed to evaluate string \"%s\"; check your syntax", s);
+
+				return r;
+			}
 		case OPR_STRING:
-			Error(LOCATION, "SEXP system does not support string return type; Goober must fix this before script-eval-string will work");
+			{
+				char* ret = NULL;
+				char* s = CTEXT(n);
+
+				bool success = Script_system.EvalString(s, "|s", &ret);
+				n = CDR(n);
+
+				if(!success)
+					Warning(LOCATION, "sexp-script-eval failed to evaluate string \"%s\"; check your syntax", s);
+
+				if (n != -1 && success)
+				{
+					Assert(Sexp_nodes[n].first == -1);
+					int variable_index = atoi(Sexp_nodes[n].text);
+
+					// verify variable set
+					Assert(Sexp_variables[variable_index].type & SEXP_VARIABLE_SET);
+
+					if (!(Sexp_variables[variable_index].type & SEXP_VARIABLE_STRING))
+					{
+						Warning(LOCATION, "Variable for script-eval has to be a string variable!");
+					}
+					else if (ret != NULL)
+					{
+						// assign to variable
+						sexp_modify_variable(ret, variable_index);
+					}
+
+					n = CDR(n);
+				}
+			}
 			break;
 		case OPR_NULL:
-			while(n != -1)
 			{
-				success = Script_system.EvalString(s, NULL, NULL, CTEXT(n));
-				n = CDR(n);
+				while (n != -1)
+				{
+					char* s = CTEXT(n);
+					bool success = Script_system.EvalString(s, NULL, NULL);
+
+					if (!success)
+						Warning(LOCATION, "sexp-script-eval failed to evaluate string \"%s\"; check your syntax", s);
+
+					n = CDR(n);
+				}
 			}
 			break;
 		default:
@@ -21498,10 +21539,7 @@ int sexp_script_eval(int node, int return_type)
 			break;
 	}
 
-	if(!success)
-		Warning(LOCATION, "sexp-script-eval failed to evaluate string \"%s\"; check your syntax", s);
-
-	return r;
+	return -1;
 }
 
 void sexp_force_glide(int node)
@@ -24893,7 +24931,6 @@ int query_operator_return_type(int op)
 		case OP_GET_OBJECT_SPEED_Y:
 		case OP_GET_OBJECT_SPEED_Z:
 		case OP_SCRIPT_EVAL_NUM:
-		case OP_SCRIPT_EVAL_STRING:
 		case OP_STRING_TO_INT:
 		case OP_GET_THROTTLE_SPEED:
 		case OP_GET_VARIABLE_BY_INDEX:
@@ -25225,6 +25262,7 @@ int query_operator_return_type(int op)
 		case OP_SET_SECONDARY_WEAPON:
 		case OP_SET_NUM_COUNTERMEASURES:
 		case OP_SCRIPT_EVAL:
+		case OP_SCRIPT_EVAL_STRING:
 		case OP_ENABLE_BUILTIN_MESSAGES:
 		case OP_DISABLE_BUILTIN_MESSAGES:
 		case OP_LOCK_PRIMARY_WEAPON:
@@ -27260,9 +27298,12 @@ int query_operator_argument_type(int op, int argnum)
 			else return OPF_NEBULA_POOF;
 
 		case OP_SCRIPT_EVAL_NUM:
-		case OP_SCRIPT_EVAL_STRING:
 		case OP_SCRIPT_EVAL:
 			return OPF_STRING;
+
+		case OP_SCRIPT_EVAL_STRING:
+			if (argnum == 1)return OPF_VARIABLE_NAME;
+			else return OPF_STRING;
 
 		case OP_CHANGE_IFF_COLOR:
 			if ((argnum == 0) || (argnum == 1))
@@ -29036,6 +29077,7 @@ int get_subcategory(int sexp_id)
 		case OP_DAMAGED_ESCORT_LIST:
 		case OP_DAMAGED_ESCORT_LIST_ALL:
 		case OP_SET_SUPPORT_SHIP:
+		case OP_SCRIPT_EVAL_STRING:
 		case OP_SCRIPT_EVAL:
 			return CHANGE_SUBCATEGORY_OTHER;
 
@@ -29135,7 +29177,6 @@ int get_subcategory(int sexp_id)
 		case OP_STRING_GET_LENGTH:
 			return STATUS_SUBCATEGORY_VARIABLES;
 
-		case OP_SCRIPT_EVAL_STRING:
 		case OP_SCRIPT_EVAL_NUM:
 			return STATUS_SUBCATEGORY_OTHER;
 			
@@ -32644,13 +32685,14 @@ sexp_help_struct Sexp_help[] = {
 
 	{OP_SCRIPT_EVAL_STRING, "script-eval-string\r\n"
 		"\tEvaluates script to return a string"
-		"Takes 1 argument...\r\n"
-		"\t1:\tScript\r\n"
+		"Takes a multiple of 2 arguments...\r\n"
+		"\t1:\tScript (Without a leading 'return')\r\n"
+		"\t2:\tDestination variable\r\n"
 	},
 
 	{OP_SCRIPT_EVAL, "script-eval\r\n"
-		"\tEvaluates script"
-		"Takes at least 1 argument...\r\n"
+		"\tEvaluates the given script\r\n"
+		"Takes 1 argument...\r\n"
 		"\t1:\tScript to evaluate\r\n"
 	},
 

commit 21e2722a74a96b2dacbe504d2ff6c073cfc0b117

Fix a few gcc compile errors introduced by r10756
git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10760 387891d4-d844-0410-90c0-e4c51a9137d3

code/mission/missionmessage.cpp
@@ -1278,6 +1278,7 @@ void message_queue_process()
 	message_q *q;
 	int i;
 	MissionMessage *m;
+	bool builtinMessage = false; // gcc doesn't like var decls crossed by goto's
 
 	// Don't play messages until first frame has been rendered
 	if ( Framecount < 2 ) {
@@ -1585,7 +1586,7 @@ void message_queue_process()
 	Script_system.SetHookVar("Message", 's', buf);
 	Script_system.SetHookVar("SenderString", 's', who_from);
 
-	bool builtinMessage = q->builtin_type != -1;
+	builtinMessage = q->builtin_type != -1;
 	Script_system.SetHookVar("Builtin", 'b', &builtinMessage);
 	if (Message_shipnum >= 0)
 	{

code/parse/lua.cpp
@@ -8739,7 +8739,7 @@ ADE_VIRTVAR(Persona, l_Message, "persona", "The persona of the message", "person
 	return ade_set_args(L, "o", l_Persona.Set(Messages[idx].persona_index));
 }
 
-ADE_FUNC(getMessage, l_Message, "[boolean replaceVars = true]", "Gets the text of the message and optionally replaces SEXP variables with their respective values.", "string", "The message or an empty string if handle is invalid", "string")
+ADE_FUNC(getMessage, l_Message, "[boolean replaceVars = true]", "Gets the text of the message and optionally replaces SEXP variables with their respective values.", "string", "The message or an empty string if handle is invalid")
 {
 	int idx = -1;
 	bool replace = true;