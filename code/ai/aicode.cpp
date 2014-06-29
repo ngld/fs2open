ngld/fs2open

commit 645aea022f526497a90d2dff06f4f3c93c1b4f0c

Fix Coverity #1093597 Negative array index read
git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10837 387891d4-d844-0410-90c0-e4c51a9137d3

code/ai/aicode.cpp
@@ -12899,6 +12899,10 @@ int ai_acquire_depart_path(object *pl_objp, int parent_objnum, int allowed_path_
 
 	// take the closest path we can find
 	ship_bay_path = ai_find_closest_depart_path(aip, pm, allowed_path_mask);
+    
+	if ( ship_bay_path <= 0 )
+		return -1;
+    
 	path_index = bay->path_indexes[ship_bay_path];
 	aip->submode_parm0 = ship_bay_path;
 	bay->depart_flags |= (1<<ship_bay_path);

commit 2e2bd753597551b67b90c11767d653ef25c18962

fix a bug introduced by r10837: 0 is a valid path index
git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10858 387891d4-d844-0410-90c0-e4c51a9137d3

code/ai/aicode.cpp
@@ -12900,7 +12900,7 @@ int ai_acquire_depart_path(object *pl_objp, int parent_objnum, int allowed_path_
 	// take the closest path we can find
 	ship_bay_path = ai_find_closest_depart_path(aip, pm, allowed_path_mask);
     
-	if ( ship_bay_path <= 0 )
+	if ( ship_bay_path < 0 )
 		return -1;
     
 	path_index = bay->path_indexes[ship_bay_path];